04fbd68e265c19befebbf81765f67385ff78538d
Trideb <panigrahitrideb1@gmail.com>
2026-03-13 09:29:09 +0530

    Updated Api URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const clientApi = Axios.create({
-  baseURL: "http://51.20.183.195:5000/api",
+  baseURL: "http://65.2.171.128:5000/api",
   timeout: 5000,
 });
 
